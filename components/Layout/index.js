c23cd7e77c736a3edfdc4a52af0110297b7bbd2d
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-08 15:54:49 +0100

    style navigation with working links and write required component tests

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import Navigation from "../Navigation";
+import styled from "styled-components";
 
 export default function Layout() {
-  return <Navigation />;
+  const StyledNav = styled.nav`
+    position: fixed;
+    bottom: 0;
+    background-color: grey;
+    height: 80px;
+    width: 100%;
+  `;
+
+  return (
+    <StyledNav>
+      <Navigation />
+    </StyledNav>
+  );
 }
--- pages/art-pieces/index.js
+++ pages/art-pieces/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ArtPieces from "../../components/ArtPieces";
 import { useContext } from "react";
 import { DataContext } from "../_app";
+import styled from "styled-components";
 
 export default function ArtPiecesPage() {
   const { data } = useContext(DataContext);
+  const StyledSection = styled.section`
+    margin-bottom: 100px;
+  `;
   return (
-    <>
+    <StyledSection>
       <ArtPieces pieces={data} />
-    </>
+    </StyledSection>
   );
 }
