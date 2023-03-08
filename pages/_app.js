dd791cc89ac17e3fa04c32f0c6395d19e24efe3e
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-08 13:51:41 +0100

    retry

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,10 @@
 import GlobalStyle from "../styles";
-import { createContext } from "react";
-import ArtPieceDataContextProvider from "../contexts/ArtPieceDataContext.js";
-
-export const ArtPiecesDataContext = createContext();
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <ArtPieceDataContextProvider>
-        <Component {...pageProps} />
-      </ArtPieceDataContextProvider>
+      <Component {...pageProps} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,13 @@ import useSWR from "swr";
 import ArtPieces from "../components/ArtPieces/index";
 import Spotlight from "../components/Spotlight";
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
   if (isLoading) {
     return <p>Loading...</p>;
   }
