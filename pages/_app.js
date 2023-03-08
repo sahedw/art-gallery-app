b82a2cb887fd1e8b8c88f548ae6fc8a8e1d8fdc5
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-08 13:49:36 +0100

    fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import GlobalStyle from "../styles";
+import { createContext } from "react";
+import ArtPieceDataContextProvider from "../contexts/ArtPieceDataContext.js";
+
+export const ArtPiecesDataContext = createContext();
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <ArtPieceDataContextProvider>
+        <Component {...pageProps} />
+      </ArtPieceDataContextProvider>
     </>
   );
 }
